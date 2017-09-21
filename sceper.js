ffb845111380e76bb608e9a6e720a9f7e0b02582
Tiemen <Tiemen2@live.nl>
2017-09-21 21:37:22 +0200

    Clean up and fixes

--- sceper.js
+++ sceper.js
@@ -1,32 +1,36 @@
 const { WebhookClient, RichEmbed } = require("discord.js")
 const fs = require("fs")
 const config = require("./config")
-const data = require("./data")
+const data = require("./data.json")
 const webhook = new WebhookClient(config.id, config.token, { disableEveryone: true })
 const RSSFeedEmitter = require("rss-feed-emitter")
 const feed = new RSSFeedEmitter()
 
 const decodeHTMLEntities = str => str.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec))
-exports.initial = true
 
-feed.add({ url: "http://sceper.ws/feed", refresh: 120000 }) // every 2 minutes
+console.log("[WEBHOOK] Ready!")
+
+feed.add({ url: "http://sceper.ws/feed", refresh: 60000 }) // every minute
 feed.on("new-item", async item => {
+    console.log("[FEED] New item")
     if (data.feed.includes(item.guid)) {
+        console.log("[DATA] Exists")
         return
     } else {
         data.feed.push(item.guid)
-        await fs.writeFileSync("./data", data)
+        await fs.writeFileSync("./data.json", JSON.stringify(data))
+        console.log("[DATA] Wrote to data.json")
     }
-    let categories = item.categories.join("| ")
-    if (!categories.includes("TV") || !categories.includes("Movie")) return // filter to only TV shows or Movies
+    let categories = item.categories.join(" | ")
+    if (!categories.includes("TV") && !categories.includes("Movie")) return // filter to only TV shows or Movies
     let color = categories.includes("TV") ? "AQUA" : categories.includes("Movie") ? "PURPLE" : "GREY"
-    let description = decodeHTMLEntities(item.description)
+    let description = decodeHTMLEntities(item.summary)
     let clearRegex = /(Release Info|Release Description.+)/gi
     description = description.replace(clearRegex, "")
     let badRegex = /((?:Release Group|Release Name|Release Date|Filename|Source|Size|Genre|Video|Audio|Subs|IMDB Rating|Runtime|Episode|Season & Episode|RT Critics|Directed By|Starring):)/gi
     description = description.replace(badRegex, "\n**$&**") // formatting
 
-    const embed = RichEmbed()
+    const embed = new RichEmbed()
         .setAuthor(categories)
         .setTitle(item.title)
         .setURL(item.link)
@@ -34,7 +38,10 @@ feed.on("new-item", async item => {
         .setDescription(description)
         .setTimestamp(item.pubDate)
 
-    webhook.send("", { embed })
+    console.log("[HOOK] Sending message")
+    webhook.send("New release!", { embeds: [embed] })
+     .then(msg => console.log("[HOOK] Message sent!"))
+     .catch(console.error)
 })
 
 process.on("unhandledRejection", err => console.log(err))
